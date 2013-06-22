fa9efca5e82a29c6da6cd554cd083c1d9e62dc0e
martin <martin@martin-ThinkPad-E420.(none)>
2013-06-22 21:47:50 +0200

    odstranenie nepotrebneho vyhladavacieho buttonu

--- gen/com/svt/candle/R.java
+++ gen/com/svt/candle/R.java
@@ -20,14 +20,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int buttonSearch=0x7f090004;
         public static final int editTextSearch=0x7f090003;
         public static final int favoritesList=0x7f090001;
         public static final int favorites_upper_text=0x7f090000;
-        public static final int menu_settings=0x7f090007;
+        public static final int menu_settings=0x7f090006;
         public static final int printCandle=0x7f090002;
-        public static final int rowTextView=0x7f090006;
-        public static final int searchList=0x7f090005;
+        public static final int rowTextView=0x7f090005;
+        public static final int searchList=0x7f090004;
     }
     public static final class layout {
         public static final int activity_favorites=0x7f030000;
