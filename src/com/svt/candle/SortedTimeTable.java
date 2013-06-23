f816941a43abbc6be1532762e5940a8ac99e429a
martin <martin@martin-ThinkPad-E420.(none)>
2013-06-23 16:02:19 +0200

    usporiadanie a pridanie prazdnych hodin

--- src/com/svt/candle/SortedTimeTable.java
+++ src/com/svt/candle/SortedTimeTable.java
@@ -10,33 +10,38 @@ public class SortedTimeTable {
 
 	TimeTable basicTimeTable = null;
 	
-	ArrayList<Lesson> pon = new ArrayList<Lesson>();
-	ArrayList<Lesson> uto = new ArrayList<Lesson>();
-	ArrayList<Lesson> str = new ArrayList<Lesson>();
-	ArrayList<Lesson> stv = new ArrayList<Lesson>();
-	ArrayList<Lesson> pia = new ArrayList<Lesson>();
+	ArrayList<ArrayList<Lesson>> days = new ArrayList<ArrayList<Lesson>>();
+	
+	
 	
 	public SortedTimeTable(TimeTable basicTimeTable) {
 		this.basicTimeTable = basicTimeTable;
+		for (int i = 0; i < 5; i++) {
+			days.add(new ArrayList<Lesson>());
+		}
 		divisionToDays();
+		for (int i = 0; i < 5; i++) {
+			sortLessonsAndAddEmpty(days.get(i));
+		}
+		
 	}
 	
 	private void divisionToDays() {
 		for (int i = 0; i < basicTimeTable.tLessons.size(); i++) {
 			if(basicTimeTable.tLessons.get(i).day.equalsIgnoreCase("pon")){
-				pon.add(basicTimeTable.tLessons.get(i));
+				days.get(0).add(basicTimeTable.tLessons.get(i));
 			}
 			if(basicTimeTable.tLessons.get(i).day.equalsIgnoreCase("uto")){
-				pon.add(basicTimeTable.tLessons.get(i));
+				days.get(1).add(basicTimeTable.tLessons.get(i));
 			}
 			if(basicTimeTable.tLessons.get(i).day.equalsIgnoreCase("str")){
-				pon.add(basicTimeTable.tLessons.get(i));
+				days.get(2).add(basicTimeTable.tLessons.get(i));
 			}
 			if(basicTimeTable.tLessons.get(i).day.equalsIgnoreCase("stv")){
-				pon.add(basicTimeTable.tLessons.get(i));
+				days.get(3).add(basicTimeTable.tLessons.get(i));
 			}
 			if(basicTimeTable.tLessons.get(i).day.equalsIgnoreCase("pia")){
-				pon.add(basicTimeTable.tLessons.get(i));
+				days.get(4).add(basicTimeTable.tLessons.get(i));
 			}
 		}
 	}
@@ -47,13 +52,16 @@ public class SortedTimeTable {
 	    	return Integer.parseInt(l1.from) - Integer.parseInt(l2.from);
 	    }
 	}
-	private ArrayList<Lesson> sortLessonsAndAddEmpty(ArrayList<Lesson> list) {
+	private void sortLessonsAndAddEmpty(ArrayList<Lesson> list) {
 		Collections.sort(list, new LessonComparator());
-		for (int i = 1; i < list.size(); i++) {
-			
+		//zaciatocnu pridat
+		for (int i = 0; i < list.size()-1; i++) {
+			int r = Integer.parseInt(list.get(i+1).from) - Integer.parseInt(list.get(i).from);
+			if(r > 0){
+				list.add(new Lesson(null, list.get(i).to, list.get(i+1).from, r, null, null, "empty", null));
+			}
 		}
-		
-		return list;
+		Collections.sort(list, new LessonComparator());
 	}
 	
 }
