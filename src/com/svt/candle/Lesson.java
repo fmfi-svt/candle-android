34f920fa97e306027a3d7145da09b3b985a4f69b
martin <martin@martin-ThinkPad-E420.(none)>
2013-06-24 09:17:21 +0200

    poopravene vykreslovanie hodin

--- src/com/svt/candle/Lesson.java
+++ src/com/svt/candle/Lesson.java
@@ -2,8 +2,11 @@ package com.svt.candle;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.TextView;
 
 /**
@@ -36,13 +39,25 @@ public class Lesson {
 	}
 
 	public View getView(Context context){
-		View view = new View(context);
 		LayoutInflater inflater = ((Activity)context).getLayoutInflater();
 		View lessonLayout = inflater.inflate(R.layout.lesson, null);
 		TextView name = (TextView) lessonLayout.findViewById(R.id.lesson_subject_name);
+//		name.setText(Integer.toString(this.duration));
 		name.setText(this.subjectName);
 		TextView room = (TextView) lessonLayout.findViewById(R.id.lesson_room);
 		room.setText(this.room);
+		DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+		int height = metrics.heightPixels;
+		int width = metrics.widthPixels;
+		if(this.subjectName.equalsIgnoreCase("empty")){
+//			lessonLayout.setLayoutParams(new LayoutParams(width/5, (R.dimen.one_lesson_height*(this.duration/60))));
+			lessonLayout.setLayoutParams(new LayoutParams((width/12*(this.duration/60)), height/5));
+		}else {
+//			lessonLayout.setLayoutParams(new LayoutParams(width/5, (R.dimen.one_lesson_height*(this.duration))));
+			lessonLayout.setLayoutParams(new LayoutParams((width/12*(this.duration)), height/5));
+		}
+		
+		
 		return lessonLayout;
 	}
 }
